05160a323aa42bfafd834b3eb06adbc2fdf184a6
j-lrx <49448040+j-lrx@users.noreply.github.com>
2023-02-22 15:48:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, New onepoint!'
+    return 'Hello, World!'
+
+@app.route('/onepoint')
+def hello_world():
+    return 'Hello, onepoint'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
